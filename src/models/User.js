4ef1118256aa5d53e4b7628ee49d95672a67ea04
Mickael <mickael.doranges@gmail.com>
2020-11-07 09:19:59 +0100

    initial commit

---
+++ src/models/User.js
@@ -0,0 +1,54 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+});
+
+userSchema.pre('save', function(next) {
+    const user = this;
+    //si l'utilisateur n'a pas modifié son mdp, ne pas ajouter de salt
+    if (!user.isModified('password')) {
+        return next();
+    }
+    bcrypt.genSalt(10, (err, salt) => {
+        if (err) {
+            return next(err);
+        }
+
+        bcrypt.hash(user.password, salt, (err, hash) => {
+            if (err) {
+                return next(err);
+            }
+            user.password = hash;
+            next();
+        });
+    });
+});
+
+userSchema.methods.comparePassword = function(candidatePassword) {
+    const user = this;
+    // on utilise la promesse pour utiliser la syntaxe (async/await) à chaque fois 
+    // qu'on compare le mot de passe
+    return new Promise((resolve, reject) => {
+        bcrypt.compare(candidatePassword, user.password, (err, isMatch) => {
+            if (err) {
+                return reject(err);
+            }
+            if (!isMatch) {
+                return reject(false);
+            }
+            resolve(true);
+        });
+    });
+}
+
+mongoose.model('User', userSchema);
